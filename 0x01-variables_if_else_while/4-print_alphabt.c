867de9c21aa98c32e956827ba05aeff5c9e4d79c
Olawale Olayigbade <olayigbadeolawale@yahoo.com>
2022-07-03 15:34:08 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,18 @@ int main(void)
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		putchar(alpha);
+		if (alpha == 'e')
+		{
+			continue;
+		}
+		else if (alpha == 'q')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(alpha);
+		}
 	}
 	putchar('\n');
 	return (0);
